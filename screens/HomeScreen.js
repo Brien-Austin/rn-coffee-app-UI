54782e2c2d085c7337149c887d39e433b4924e39
Brien-Austin <brienaustinclayton@gmail.com>
2023-11-12 13:25:47 +0530

    added navbar and categories

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,19 +1,99 @@
-import { View, Text, SafeAreaView, TouchableOpacity } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+  TextInput,
+  FlatList,
+} from "react-native";
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Ionicons } from "@expo/vector-icons";
+import { categories } from "../constants";
 
+import { themeColors } from "../theme";
 const HomeScreen = () => {
+  const [activeCategory, setActiveCategory] = useState(1);
   return (
-    <SafeAreaView className="flex-1 justify-center items-center bg-blue-100">
-      <View className="bg-black w-4/5 rounded-lg flex flex-column justify-center items-center">
-        <Text className="text-white p-5">
-          Hey !!! A React Native Expo Tailwind Template with React Navigation as
-          well
-        </Text>
-        <TouchableOpacity className="w-3/5 rounded-lg h-8 bg-white flex justify-center items-center mb-2 ">
-          <Text className="text-black text-[16px]">Try Tailwind !!</Text>
-        </TouchableOpacity>
-      </View>
-    </SafeAreaView>
+    <View>
+      <StatusBar />
+      <Image
+        source={require("../assets/images/beansBackground1.png")}
+        className="w-full absolute -top-5 opacity-10"
+        style={{ height: 220 }}
+      />
+      <SafeAreaView>
+        <View className="mt-14 px-4 flex-row justify-between items-center">
+          <Image
+            source={require("../assets/images/avatar.png")}
+            className=" h-9 w-9 rounded-full"
+          />
+          <View className="flex-row space-x-2 items-center">
+            <Ionicons
+              size={24}
+              name="location-outline"
+              color={themeColors.bgLight}
+            />
+            <Text className="text-base font-semibold">New York,NYC</Text>
+          </View>
+
+          <Ionicons name="notifications-outline" size={30} color={"black"} />
+        </View>
+
+        {
+          // Search Bar
+        }
+
+        <View className="mx-5  mt-14">
+          <View className="flex-row justify-center items-center rounded-full p-1 bg-[#e6e6e6]">
+            <TextInput
+              className="p-3 flex-1 font-semibold text-gray-700"
+              placeholder="Search"
+            />
+            <TouchableOpacity
+              style={{ backgroundColor: themeColors.bgLight }}
+              className="rounded-full p-2"
+            >
+              <Ionicons size={27} name="search" color={"white"} />
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        {
+          // Categories
+        }
+
+        <View className="px-5 mt-6">
+          <FlatList
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            data={categories}
+            keyExtractor={(item) => item.id}
+            className="overflow-visible"
+            renderItem={({ item }) => {
+              isActive = item.id == activeCategory;
+              let activeTextClass = isActive ? "text-white" : "text-gray-700";
+              return (
+                <TouchableOpacity
+                  onPress={() => setActiveCategory(item.id)}
+                  style={{
+                    backgroundColor: isActive
+                      ? themeColors.bgLight
+                      : "rgba(0,0,0,0.07)",
+                  }}
+                  className="p-4 px-5 mr-2 rounded-full shadow"
+                >
+                  <Text className={"font-semibold " + activeTextClass}>
+                    {item.title}
+                  </Text>
+                </TouchableOpacity>
+              );
+            }}
+          />
+        </View>
+      </SafeAreaView>
+    </View>
   );
 };
 
